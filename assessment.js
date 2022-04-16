24d7b3501b84e16b410c01c368feaf5e6039c1d4
kusutona <103849008+kustona@users.noreply.github.com>
2022-04-17 05:13:10 +0900

    bootstrapによりデザインを改良

--- assessment.js
+++ assessment.js
@@ -9,14 +9,25 @@ assessmentButton.onclick = () => {
     if(userName.length===0) return;
     console.log(userName);
     resultDivided.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivided.appendChild(header);
+
+    const headerDivided = document.createElement('div');
+    headerDivided.setAttribute('class', 'card-header');
+    headerDivided.innerText = '診断結果'
+
+    const bodyDivided = document.createElement('div');
+    bodyDivided.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivided.appendChild(paragraph);
+    bodyDivided.appendChild(paragraph);
+
+    resultDivided.setAttribute('class', 'card');
+    resultDivided.setAttribute('style', 'max-width: 700px;')
+
+    resultDivided.appendChild(headerDivided);
+    resultDivided.appendChild(bodyDivided);
 
     tweetDivided.innerText = '';
     const anchor = document.createElement('a');
